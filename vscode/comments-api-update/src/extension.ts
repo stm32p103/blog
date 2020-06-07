b91c8cf680dcaf1468fd7ad117d1a2f719906d16
stm32p103 <stm32p103@gmail.com>
2020-06-07 14:34:39 +0900

    Comments API コメントの操作お試し

--- vscode/comments-api-update/src/extension.ts
+++ vscode/comments-api-update/src/extension.ts
@@ -1,27 +1,103 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+class MyComment implements vscode.Comment {
+	static count: number = 0;
+	lastPublished: string | vscode.MarkdownString = '';
+
+	constructor( private readonly thread: vscode.CommentThread,
+		public author: vscode.CommentAuthorInformation,
+		public body: string | vscode.MarkdownString = '',
+		public mode: vscode.CommentMode = vscode.CommentMode.Preview,
+		public contextValue?: string,
+		public reactions?: vscode.CommentReaction[],
+		public label?: string,
+	) {}
+
+	private updateThread() {
+		this.thread.comments = this.thread.comments;
+	}
+
+	startEdit() {
+		this.lastPublished = this.body;
+		this.mode = vscode.CommentMode.Editing;
+		this.updateThread();
+	}
+
+	applyEdit() {
+		this.lastPublished = '';
+		this.mode = vscode.CommentMode.Preview;
+		this.updateThread();
+	}
+
+	cancelEdit() {
+		this.body = this.lastPublished;
+		this.lastPublished = '';
+		this.mode = vscode.CommentMode.Preview;
+		this.updateThread();
+	}
+
+	removeFromThread() {
+		this.thread.comments = this.thread.comments.filter( comment => comment !== this );
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
+	const commentController = vscode.comments.createCommentController( 'SampleCommentController', 'Sample Comments' );
+	
+	// create comment on current selection
+	let disposable = vscode.commands.registerCommand('create.commentThread', () => {
+		const uri = vscode.window.activeTextEditor?.document.uri;
+    const selected = vscode.window.activeTextEditor?.selection;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "comments-api-update" is now active!');
+    if( uri && selected ) {
+			const thread = commentController.createCommentThread( uri, selected, [] );
+			thread.collapsibleState = vscode.CommentThreadCollapsibleState.Expanded;
+    }
+	} );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('comments-api-update.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	vscode.commands.registerCommand('remove.commentThread', ( thread: vscode.CommentThread ) => {
+		if( !thread ) {
+			return;
+		}
+		thread.dispose();
+	}, disposable );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Comments API - How to update comments?!');
-	});
+  vscode.commands.registerCommand( 'reply.commentThread', ( reply: vscode.CommentReply ) => {
+		if( !reply ) {
+			return;
+		}
+		reply.thread.comments = reply.thread.comments.concat( new MyComment( reply.thread, { name: 'Anonymous' }, reply.text ) );
+	}, disposable );
+
+  vscode.commands.registerCommand( 'start.edit.comment', ( comment: MyComment ) => {
+		if( !comment ) {
+			return;
+		}
+		comment.startEdit();
+	}, disposable );
+
+  vscode.commands.registerCommand( 'apply.edit.comment', ( comment: MyComment ) => {
+		if( !comment ) {
+			return;
+		}
+		comment.applyEdit();
+	}, disposable );
+	
+  vscode.commands.registerCommand( 'cancel.edit.comment', ( comment: MyComment ) => {
+		if( !comment ) {
+			return;
+		}
+		comment.cancelEdit();
+	}, disposable );
+	
+  vscode.commands.registerCommand( 'remove.comment', ( comment: MyComment ) => {
+		if( !comment ) {
+			return;
+		}
+		comment.removeFromThread();
+	}, disposable );
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
